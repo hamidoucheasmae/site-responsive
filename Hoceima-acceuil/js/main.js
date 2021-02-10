dcc76c871422b8fafbf20d56ec26759ad13bb136
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2021-02-10 10:42:03 +0100

    add dossier hoceima-acceuil (qui contient la version améliore de la page accueil)

---
+++ Hoceima-acceuil/js/main.js
@@ -0,0 +1,12 @@
+// var links = Document.querySelectorAll('.links a'  )
+// for (var i = 0; i < links.length; i++){
+//     var link = links[i]
+//     link.style.color = "#FFA500";
+// }
+
+
+
+
+// $('.links a').css('color', '#FFA500')
+
+
